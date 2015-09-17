8ec53f278450e379514965b9b09a4dd9bc33ea4f
Copy Liu <copyliu@gmail.com>
2015-09-17 23:22:59 +0800

    static files path

--- csvt01/settings.py
+++ csvt01/settings.py
@@ -103,8 +103,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    'C:\\Users\\Kouen\\Desktop\\majiangcalc\\blog\\static',
+    os.path.join(BASE_DIR, "blog","static"),
 )
 
 
